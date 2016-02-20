14100c5602d89aa4f67b6d11b0c107cf09b84c7e
Mike Mahoney <mahonmr@me.com>
2016-02-19 22:16:16 -0800

    small fixes to display images properly

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,21 +1,64 @@
-$(document).ready(function(){
-  $('#gituser').click(function(event){
-    event.preventDefault();
-    $('.container2').html('<div id="loader"><img src="css/loader.gif" alt="loading..."></div>');
+$(function(){
+  $('#ghsubmitbtn').on('click', function(e){
+    e.preventDefault();
+    $('#ghapidata').html('<div id="loader"><img src="./build/css/loader.gif" alt="loading..."></div>');
 
-    var coder = $("input#coder").val();
-    var gitCoder   = 'https://api.github.com/users/'+coder;
-    var gitCoderRepo  = 'https://api.github.com/users/'+coder+'/repos';
+    var username = $('#ghusername').val();
+    var requri   = 'https://api.github.com/users/'+username;
+    var repouri  = 'https://api.github.com/users/'+username+'/repos';
 
-    $('input#coder').val("");
-console.log(coder);
-    $.get('https://api.github.com/users/'+coder, function(response) {
-      if (response.status !== 200) {
-        $('.container2').text("aldfjadlkj");
-      } else {
-        $('.container2').text("The humidity");
+    requestJSON(requri, function(json) {
+      if(json.message == "Not Found" || username == '') {
+        $('#ghapidata').html("<h2>No User Info Found</h2>");
+      }
+
+      else {
+        // else we have a user and we display their info
+        var fullname   = json.name;
+        var username   = json.login;
+        var aviurl     = json.avatar_url;
+        var profileurl = json.html_url;
+        var location   = json.location;
+        var followersnum = json.followers;
+        var followingnum = json.following;
+        var reposnum     = json.public_repos;
+
+        if(fullname == undefined) { fullname = username; }
+
+        var outhtml = '<h2>'+fullname+' <span class="smallname">(@<a href="'+profileurl+'" target="_blank">'+username+'</a>)</span></h2>';
+        outhtml = outhtml + '<div class="ghcontent"><div class="avi"><a href="'+profileurl+'" target="_blank"><img src="'+aviurl+'" width="80" height="80" alt="'+username+'"></a></div>';
+        outhtml = outhtml + '<p>Followers: '+followersnum+' - Following: '+followingnum+'<br>Repos: '+reposnum+'</p></div>';
+        outhtml = outhtml + '<div class="repolist clearfix">';
+
+        var repositories;
+        $.getJSON(repouri, function(json){
+          repositories = json;
+          outputPageContent();
+        });
+
+        function outputPageContent() {
+          if(repositories.length == 0) { outhtml = outhtml + '<p>No repos!</p></div>'; }
+          else {
+            outhtml = outhtml + '<p><strong>Repos List:</strong></p> <ul>';
+            $.each(repositories, function(index) {
+              outhtml = outhtml + '<li><a href="'+repositories[index].html_url+'" target="_blank">'+repositories[index].name + '</a></li>';
+            });
+            outhtml = outhtml + '</ul></div>';
+          }
+          $('#ghapidata').html(outhtml);
+        } // end outputPageContent()
+      } // end else statement
+    }); // end requestJSON Ajax call
+  }); // end click event handler
+
+  function requestJSON(url, callback) {
+    $.ajax({
+      url: url,
+      complete: function(xhr) {
+        callback.call(null, xhr.responseJSON);
       }
     });
-  });
+  }
 });
+
 
